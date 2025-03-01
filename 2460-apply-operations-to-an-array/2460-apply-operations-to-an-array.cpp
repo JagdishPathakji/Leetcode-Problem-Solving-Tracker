caf04fca1b84ae004360787163e4be5b6bc2eb0d
JagdishPathakji <jagdishgithub1@gmail.com>
2025-03-01 21:54:42 +0530

    Time: 0 ms (100%), Space: 13.1 MB (62.06%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -10,13 +10,18 @@ public:
                 nums[i+1] = 0;
             }
         }   
-        
-        for(int i=nums.size()-1; i>0; i--) {
-            if(nums[i-1] == 0) {
-                swap(nums[i],nums[i-1]);
+
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] != 0) {
+                v.push_back(nums[i]);
             }
         }
 
-        return nums;
+        for(int i=v.size(); i<nums.size(); i++) {
+            v.push_back(0);
+        }
+
+
+        return v;
     }
 };
