8b4e4c283c71236ed00cebafbbfec4b15ce79e3b
JagdishPathakji <jagdishgithub1@gmail.com>
2025-05-19 19:23:15 +0530

    Time: 16 ms (90.89%) | Memory: 65.7 MB (79.65%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+
+        vector<vector<int>> adjlist(numCourses);
+        for(int i=0; i<prerequisites.size(); i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+
+            adjlist[u].push_back(v);
+        }
+
+        vector<int> indegree(numCourses,0);
+        for(int i=0; i<numCourses; i++) {
+            for(int j=0; j<adjlist[i].size(); j++)
+                indegree[adjlist[i][j]]++;
+        }
+
+        queue<int> q;
+        for(int i=0; i<numCourses; i++) {
+            if(indegree[i] == 0)
+            q.push(i);
+        }
+
+        vector<vector<bool>> dependency(numCourses, vector<bool>(numCourses,false));
+        while(!q.empty()) {
+
+            int node = q.front();
+            q.pop();
+
+            for(int i=0; i<adjlist[node].size(); i++) {
+                dependency[adjlist[node][i]][node] = 1;
+                for(int j=0; j<numCourses; j++) {
+                    if(dependency[node][j])
+                    dependency[adjlist[node][i]][j] = 1;
+                }
+
+                indegree[adjlist[node][i]]--;
+                if(indegree[adjlist[node][i]] == 0)
+                q.push(adjlist[node][i]);
+            }
+        }
+
+        vector<bool> result(queries.size(),false);
+        for(int i=0; i<queries.size(); i++) {
+            result[i] = dependency[queries[i][1]][queries[i][0]];
+        }
+
+        return result;
+    }
+};
