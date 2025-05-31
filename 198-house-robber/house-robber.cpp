e0890f32b7ae1b8693f15b16701f13442f39f45a
JagdishPathakji <jagdishgithub1@gmail.com>
2025-05-31 11:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.91%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,18 +3,23 @@ public:
 
     int rob(vector<int>& nums) {
 
+        if(nums.size() == 0)
+        return nums[0];
+
         int n = nums.size();
-        vector<int> dp(n+1);
         // dp[i] = max stolen money still i th house
-        dp[0] = 0; // when i have robbed zero houses, then ofcourse the total money robbed till now is zero
-        dp[1] = nums[0]; // when i have robbed 1st house, maxm money i can rob is nums[0]
+        int one = 0; // when i have robbed zero houses, then ofcourse the total money robbed till now is zero
+        int two = nums[0]; // when i have robbed 1st house, maxm money i can rob is nums[0]
 
         for(int i=2; i<=n; i++) {
-            int steal = nums[i-1] + dp[i-2];
-            int skip = dp[i-1];
-            dp[i] = max(steal,skip);
+            int steal = nums[i-1] + one;
+            int skip = two;
+            int curr = max(steal,skip);
+
+            one = two;
+            two = curr;
         }
 
-        return dp[n];
+        return two;
     } 
 };
