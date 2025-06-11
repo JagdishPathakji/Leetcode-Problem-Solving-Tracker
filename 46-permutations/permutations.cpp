8fdbfbcb149f96d8df598e114537fc5807e0e801
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-06-11 18:07:45 +0530

    Time: 3 ms (30.02%) | Memory: 11.9 MB (17.70%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
 
-    void findpermutations(vector<int> &nums, int index, vector<int> &temp, vector<bool> &visited, vector<vector<int>> &ans) {
+    /*
+    void findpermutations(vector<int> &nums, vector<int> &temp, vector<bool> &visited, vector<vector<int>> &ans) {
 
         if(temp.size() == nums.size()) {
             ans.push_back(temp);
@@ -12,22 +13,45 @@ public:
             if(!visited[i]) {
                 temp.push_back(nums[i]);
                 visited[i] = 1;
-                findpermutations(nums,i,temp,visited,ans);
+                findpermutations(nums,temp,visited,ans);
                 temp.pop_back();
                 visited[i] = 0;
             }
         }
     }
+    */
+
+    void find(vector<int> nums, vector<vector<int>> &ans, int index) {
+        
+
+        if(index == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i=index; i<nums.size(); i++) {
+            swap(nums[index],nums[i]);
+            find(nums,ans,index+1);
+        }
+    }
 
     vector<vector<int>> permute(vector<int>& nums) {
         
-        int index = 0;
+        /*
         int n = nums.size();
         vector<int> temp;
         vector<vector<int>> ans;
         vector<bool> visited(n,false);
 
-        findpermutations(nums,index,temp,visited,ans);
+        findpermutations(nums,temp,visited,ans);
+
+        return ans;
+        */
+
+        int n = nums.size();    
+        vector<vector<int>> ans;
+        int index = 0;
+        find(nums,ans,index);
 
         return ans;
     }
