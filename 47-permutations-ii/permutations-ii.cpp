6800f0555cba2aa9f6104856bc09df6ffa0af7a7
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-06-11 21:14:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (42.81%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -11,10 +11,10 @@ public:
         vector<int> used(21,0);
         for(int i=index; i<nums.size(); i++) {
             if(!used[nums[i]+10]) {
+                used[nums[i]+10] = true;
                 swap(nums[i],nums[index]);
                 find(nums,index+1,ans);
                 swap(nums[i],nums[index]);
-                used[nums[i]+10] = true;
             }
         }
     }
