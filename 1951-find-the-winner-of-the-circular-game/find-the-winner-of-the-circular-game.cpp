9602ccbcf998a5ac413ac179e87f428a43fc06f3
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-06-12 02:36:44 +0530

    Time: 81 ms (5.11%) | Memory: 8.2 MB (55.86%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        
+        int i = 0;
+        vector<bool> visited(n,false);
+        int count = 0;
+
+        while(count != n-1) {
+
+            while(visited[i]) {
+                i = (i + 1) % n;
+            }
+
+            int step = 0;
+            while(step < k) {
+                if(!visited[i]) {
+                    step++;
+                }
+                if(step == k) break;
+                i = (i + 1) % n;
+            }
+            
+            visited[i] = true;
+            count++;
+        }
+
+        for(int i=0; i<n; i++) {
+            if(!visited[i])
+            return i+1;
+        }
+
+        return -1;
+    }
+};
