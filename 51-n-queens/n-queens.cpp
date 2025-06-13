04c20924a8cd5cf3f65df6a4dcec280bcd563654
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-06-13 19:55:49 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (93.05%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,36 +1,7 @@
 class Solution {
 public:
 
-    bool checkdiagonal(int i, int j, int n, vector<string> &board) {
-
-        // upper left diagonal , there must be no queen
-        int row = i;
-        int col = j;
-
-        while(row > -1 and col > -1) {
-            if(board[row][col] == 'Q')
-            return false;
-
-            row = row - 1;
-            col = col - 1;
-        }
-
-        // upper right diagonal, there must be no queen
-        row = i;
-        col = j;
-
-        while(row > -1 and col < n) {
-            if(board[row][col] == 'Q')
-            return false;
-
-            row = row - 1;
-            col = col + 1;
-        }   
-
-        return true;
-    }
-
-    void find(int row, int n, vector<string> &board, vector<vector<string>> &ans, vector<bool> &column) {
+    void find(int row, int n, vector<string> &board, vector<vector<string>> &ans, vector<bool> &column, vector<bool> &leftdiagonal, vector<bool> &rightdiagonal) {
 
         // base case
         if(row == n) {
@@ -39,12 +10,16 @@ public:
         }
 
         for(int j=0; j<n; j++) {
-            if(column[j] == 0 and checkdiagonal(row,j,n,board)) {
+            if(column[j] == 0 and leftdiagonal[(n-1)+(j-row)] == 0 and rightdiagonal[row+j] == 0) {
                 column[j] = 1;
                 board[row][j] = 'Q';
-                find(row+1,n,board,ans,column);
+                leftdiagonal[(n-1)+(j-row)] = 1;
+                rightdiagonal[row+j] = 1;
+                find(row+1,n,board,ans,column,leftdiagonal,rightdiagonal);
                 board[row][j] = '.';
                 column[j] = 0;
+                leftdiagonal[(n-1)+(j-row)] = 0;
+                rightdiagonal[row+j] = 0;
             }
         }
     }
@@ -53,17 +28,19 @@ public:
         
         vector<vector<string>> ans;
         vector<string> board(n);
+        vector<bool> leftdiagonal(2*n-1,false);
+        vector<bool> rightdiagonal(2*n-1,false);
         
+
         for(int i=0; i<n; i++)
         for(int j=0; j<n; j++) 
         board[i].push_back('.');
 
         // to check two queens not in same col.
-        vector<bool> column(n,false);
         // for row it can be done without extra vector.
-        // for diagonal we need to check manually using loop
+        vector<bool> column(n,false);
 
-        find(0,n,board,ans,column);
+        find(0,n,board,ans,column,leftdiagonal,rightdiagonal);
 
         return ans;
     }
