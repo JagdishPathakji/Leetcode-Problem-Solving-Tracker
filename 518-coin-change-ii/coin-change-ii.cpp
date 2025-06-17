59bc230118adff0508222c342a11d8e453475eba
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-06-17 23:34:34 +0530

    Time: 18 ms (59.34%) | Memory: 28.9 MB (28.13%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -3,7 +3,7 @@ public:
 
     int find(int index, vector<int> &coins, int amount, vector<vector<int>> &dp) {
 
-        if(index < 0 || amount < 0) 
+        if(index < 0) 
         return 0;
 
         if(amount == 0)
@@ -12,6 +12,9 @@ public:
         if(dp[index][amount] != -1)
         return dp[index][amount];
 
+        if(coins[index] > amount) // replacement for if(amount < 0) return 0;
+        return dp[index][amount] = find(index-1,coins,amount,dp);
+        else
         return dp[index][amount] = find(index,coins,amount-coins[index],dp) + find(index-1,coins,amount,dp);
     }
 
@@ -19,7 +22,7 @@ public:
         
         int n = coins.size();
         vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+
         return find(n-1,coins,amount,dp);
-        
     }
 };
