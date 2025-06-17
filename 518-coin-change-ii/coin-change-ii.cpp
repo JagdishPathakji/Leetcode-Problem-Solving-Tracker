d2f6067d3bbf2f15db107e68522e02081fb1a094
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-06-17 21:59:21 +0530

    Time: 19 ms (55.38%) | Memory: 28.8 MB (30.65%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -3,28 +3,23 @@ public:
 
     int find(int index, vector<int> &coins, int amount, vector<vector<int>> &dp) {
 
-        if(index >= coins.size()) 
+        if(index < 0 || amount < 0) 
         return 0;
 
-        if(amount < 0)
-        return 0;
-
-        if(amount == 0) return 1;
+        if(amount == 0)
+        return 1;
 
         if(dp[index][amount] != -1)
         return dp[index][amount];
 
-        int takecurrent = find(index,coins,amount-coins[index],dp);
-        int moveforward = find(index+1,coins,amount,dp);
-
-        return dp[index][amount] = (takecurrent+moveforward);
+        return dp[index][amount] = find(index,coins,amount-coins[index],dp) + find(index-1,coins,amount,dp);
     }
 
     int change(int amount, vector<int>& coins) {
         
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int>(amount+1,-1));
-        return find(0,coins,amount,dp);
-
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        return find(n-1,coins,amount,dp);
+        
     }
 };
