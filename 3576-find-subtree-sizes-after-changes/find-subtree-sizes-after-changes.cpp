350807944cb488ccfc6a53a8a85a9a608e468844
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-07-04 23:10:31 +0530

    Time: 514 ms (52.75%) | Memory: 292.5 MB (43.88%) - LeetSync

--- 3576-find-subtree-sizes-after-changes/find-subtree-sizes-after-changes.cpp
+++ 3576-find-subtree-sizes-after-changes/find-subtree-sizes-after-changes.cpp
@@ -10,10 +10,12 @@ public:
         }
     }
 
-    void dfs(int node, vector<int> &parent, vector<vector<int>> &graph, string &s, unordered_map<char,int> &ancestors) {
+    void DFS(int node, vector<int> &parent, vector<vector<int>> &graph, string &s, unordered_map<char,int> &ancestors) {
 
         char ch = s[node];
 
+        // If an ancestor with the same character s[node] exists, update parent[node] to point to that ancestor.
+        // Uses unordered_map<char, int> to remember the latest node with a given character on the path from root.
         if(ancestors.count(ch)) {
             parent[node] = ancestors[ch];
         }
@@ -21,16 +23,18 @@ public:
         int original = ancestors.count(ch) ? ancestors[ch] : -1;
         ancestors[ch] = node;
 
+        // Apply DFS on the chilrens
         for(int child : graph[node]) {
-            dfs(child,parent,graph,s,ancestors);
-        }        
+            DFS(child,parent,graph,s,ancestors);
+        }
 
         if(original == -1) {
             ancestors.erase(ch);
         }
         else {
-            ancestors[ch] = original; 
+            ancestors[ch] = original;
         }
+
     }
 
     int findsize(int node, vector<vector<int>> &graph, vector<int> &ans) {
@@ -40,24 +44,33 @@ public:
         for(int child : graph[node]) {
             size += findsize(child,graph,ans);
         }
+
         ans[node] = size;
         return size;
     }
 
     vector<int> findSubtreeSizes(vector<int>& parent, string s) {
 
+        /* Steps : 
+        1. Build the graph
+        2. Perform DFS and change the parent of each node to its immediate parent with same s[node] == s[parent]
+        3. Build the new graph with new parents obtained in above DFS
+        4. Find out the size of each subtree
+        */        
+
         int n = parent.size();
-        vector<vector<int>> graph(n);
-        vector<int> ans(n);
-        unordered_map<char,int> ancestors;
+        vector<int> ans(n,0); // answer to be returned
+        vector<vector<int>> graph(n); // n vertices
+        unordered_map<char,int> ancestors; // help to find out the immediate parent with same s[node] == s[parent] during DFS.
 
-        buildgraph(parent,graph);
-        dfs(0,parent,graph,s,ancestors);
+        buildgraph(parent,graph); // step 1
+        DFS(0,parent,graph,s,ancestors); // step 2
 
+        // re-initialize the graph to not override the previous information
         graph = vector<vector<int>>(n);
-        buildgraph(parent,graph);
-        findsize(0,graph,ans);
-        
+        buildgraph(parent,graph); // step 3
+        findsize(0,graph,ans); // step 4
+
         return ans;
     }
 };
