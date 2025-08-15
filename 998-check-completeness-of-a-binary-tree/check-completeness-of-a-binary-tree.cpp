66ed383dd44e1473dc7c49478a637e413d2dc232
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-08-15 20:11:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (24.87%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -12,43 +12,72 @@
 class Solution {
 public:
 
-    void find(TreeNode *root, int &size) {
+    // void find(TreeNode *root, int &size) {
 
-        if(!root)
-        return;
+    //     if(!root)
+    //     return;
 
-        size++;
+    //     size++;
 
-        find(root->left,size);
-        find(root->right,size);
-    }
+    //     find(root->left,size);
+    //     find(root->right,size);
+    // }
 
-    void traverse(TreeNode *root, int index, int &size, bool &ans) {
+    // void traverse(TreeNode *root, int index, int &size, bool &ans) {
 
-        if(!root)
-        return;
+    //     if(!root)
+    //     return;
 
-        if(!ans)
-        return;
+    //     if(!ans)
+    //     return;
 
-        if(index >= size) {
-            ans = false;
-            return;
-        }
+    //     if(index >= size) {
+    //         ans = false;
+    //         return;
+    //     }
 
-        traverse(root->left,2*index+1,size,ans);
-        traverse(root->right,2*index+2,size,ans);
-    }
+    //     traverse(root->left,2*index+1,size,ans);
+    //     traverse(root->right,2*index+2,size,ans);
+    // }
 
     bool isCompleteTree(TreeNode* root) {
         
-        int size = 0;
-        find(root,size);
+        // DFS Approach
+        // BFS Approach: level order traversal
+        // int size = 0;
+        // find(root,size);
 
-        int index = 0;
-        bool ans = true;
-        traverse(root,index,size,ans);
+        // int index = 0;
+        // bool ans = true;
+        // traverse(root,index,size,ans);
 
-        return ans;
+        // return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        bool pastNullNodeCheck = false;
+
+        while(!q.empty()) {
+
+            auto node = q.front();
+            q.pop();
+
+            if(node == nullptr) {
+                pastNullNodeCheck = true;
+            }
+            else {
+
+                if(pastNullNodeCheck) 
+                return false;
+            
+                q.push(node->left);
+                q.push(node->right);    
+
+            }
+
+        }
+
+        return true;
     }
 };
