720b6679610db945bcd23916e49ce22f38f5e2db
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-08-16 06:11:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (47.31%) - LeetSync

---
+++ 832-binary-tree-pruning/binary-tree-pruning.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        
+        if(!root)
+        return nullptr;
+
+        if(!root->left and !root->right) {
+            if(root->val == 1)
+            return root;
+            else
+            return nullptr;
+        }
+
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+
+        if(root->val == 1)
+        return root;
+
+        if(!root->left && !root->right)
+        return nullptr;
+
+        if(root->left && root->right)
+        return root;
+
+        if(root->left || root->right)
+        return root;
+
+        return nullptr;
+    }
+};
