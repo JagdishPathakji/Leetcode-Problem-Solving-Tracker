f9ffc33f10186c81d416852be6860553c3d8e87e
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-09-30 15:29:20 +0530

    Time: 3 ms (83.27%) | Memory: 28.6 MB (12.86%) - LeetSync

--- 1767-design-front-middle-back-queue/design-front-middle-back-queue.cpp
+++ 1767-design-front-middle-back-queue/design-front-middle-back-queue.cpp
@@ -1,155 +1,110 @@
-class node {
-public:
-    int data;
-    node *next;
-    node *prev;
-
-    node(int data) {
-        this->data = data;
-        next = prev = nullptr;
-    }
-};
-
 class FrontMiddleBackQueue {
 public:
 
-    node *head;
-    node *tail;
-    int size;
+    deque<int> d1;
+    deque<int> d2;
+
     FrontMiddleBackQueue() {
-        head = nullptr;
-        tail = nullptr;
-        size = 0;
+        
     }
     
     void pushFront(int val) {
-        node *element = new node(val);
-        
-        if(!head) {
-            head = element;
-            tail = element;
-        }
+        if(d1.size() == d2.size()) {
+            d1.push_front(val);
+        }    
         else {
-            element->next = head;
-            head->prev = element;
-            head = element;
+            d2.push_front(d1.back());
+            d1.pop_back();
+            d1.push_front(val);
         }
-        size++;
     }
     
     void pushMiddle(int val) {
-        node *element = new node(val);
-        int iteration = size/2;
-
-        node *curr = head;
-        node *pre = nullptr;
-        while(curr and iteration--) {
-            pre = curr;
-            curr = curr->next;
-        }
-
-        if(pre) {
-            pre->next = element;
-            element->prev = pre;
-            element->next = curr;
-            if(curr) 
-            curr->prev = element;
-            else
-            tail = element;
+        if(d1.size() == d2.size()) {
+            d1.push_back(val);
         }
         else {
-            element->next = head;
-            
-            if(head)
-            head->prev = element;    
-            else
-            tail = element;
-            
-            head = element;
+            d2.push_front(d1.back());
+            d1.pop_back();
+            d1.push_back(val);
         }
-
-
-        size++;
     }
     
     void pushBack(int val) {
-        node *element = new node(val);
-
-        if(!head) {
-            head = element;
-            tail = element;
+        if(d1.size() == d2.size()) {
+            d2.push_back(val);
+            d1.push_back(d2.front());
+            d2.pop_front();
         }
         else {
-            tail->next = element;
-            element->prev = tail;
-            tail = tail->next;
+            d2.push_back(val);
         }
-        size++;
     }
     
     int popFront() {
         
-        if(!head) {
-            return -1;
+        if(d1.size() == 0)
+        return -1;
+
+        int el;
+        if(d1.size() == d2.size()) {
+            el = d1.front();
+            d1.pop_front();
+            d1.push_back(d2.front());
+            d2.pop_front();
+        }
+        else {
+            el = d1.front();
+            d1.pop_front();
         }
 
-        int data = head->data;
-        head = head->next;
-        
-        if(head)
-        head->prev = nullptr;
-        else
-        tail = nullptr;
-
-        size--;
-        return data;
+        return el;
     }
     
     int popMiddle() {
         
-        if(!head) {
-            return -1;
+        if(d1.size() == 0)
+        return -1;
+
+        int el;
+        if(d1.size() == d2.size()) {
+            el = d1.back();
+            d1.pop_back();
+            d1.push_back(d2.front());
+            d2.pop_front();
         }
-        
-        node *curr = head;
-        int iteration = (size-1)/2;
-        
-        while(iteration--) {
-            curr = curr->next;
-        }   
+        else {
+            el = d1.back();
+            d1.pop_back();
+        }
 
-        int data = curr->data;
-        
-        if(curr->prev) 
-        curr->prev->next = curr->next;
-        else
-        head = curr->next;
-
-        if(curr->next)
-        curr->next->prev = curr->prev;
-        else
-        tail = curr->prev;
-
-        size--;
-        return data;
+        return el;
     }
     
     int popBack() {
-        
-        if(!head) {
-            return -1;
+
+        int el;
+        if(d1.size() != 0 && d2.size() == 0) {
+            el = d1.back();
+            d1.pop_back();
+            return el;
         }
 
-        int data = tail->data;
-        
-        tail = tail->prev;
-        if(tail)
-        tail->next = nullptr;
-        else
-        head = nullptr;
+        if(d1.size() == 0 && d2.size() == 0) 
+        return -1;
 
-        size--;
-        return data;
+        if(d1.size() == d2.size()) {
+           el = d2.back();
+           d2.pop_back();
+        }
+        else {
+            el = d2.back();
+            d2.pop_back();
+            d2.push_front(d1.back());
+            d1.pop_back();
+        }
+
+        return el;
     }
 };
 
