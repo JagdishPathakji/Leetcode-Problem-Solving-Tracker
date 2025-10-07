07c6e8a61162af57041baa0bb25296f722a03aee
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-10-07 11:43:53 +0530

    Time: 39 ms (16.76%) | Memory: 61.8 MB (5.33%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
 
-    int find(int index, vector<int> &coins, int amount, vector<vector<int>> &dp) {
-
-        if(index <= 0) 
-        return 0;
-
-        if(amount == 0)
-        return 1;
-
-        if(dp[index][amount] != -1)
-        return dp[index][amount];
-
-        if(coins[index-1] > amount) // replacement for if(amount < 0) return 0;
-        return dp[index][amount] = find(index-1,coins,amount,dp);
-        else
-        return dp[index][amount] = find(index,coins,amount-coins[index-1],dp) + find(index-1,coins,amount,dp);
-    }
 
     int change(int amount, vector<int>& coins) {
         
         int n = coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+        vector<vector<long long int>> dp(n+1,vector<long long int>(amount+1,0));
 
-        return find(n,coins,amount,dp);
+        // if(amount == 0) return 1;
+        for(int i=0; i<=n; i++)
+        dp[i][0] = 1;
+        
+        // if(index <= 0) automatically handled as dp is initialised with zero.
 
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=amount; j++) {
+                if(coins[i-1] > j)
+                dp[i][j] = dp[i-1][j];
+                else
+                dp[i][j] = int(dp[i][j-coins[i-1]] + dp[i-1][j]);
+            }
+        }
+
+        return (int)dp[n][amount];
     }
 };
