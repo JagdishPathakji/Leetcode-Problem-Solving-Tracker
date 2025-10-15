3ee0cedcd5cac420846f1e67673b4d4943d9e803
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-10-15 19:03:36 +0530

    Time: 3 ms (96.09%) | Memory: 28.8 MB (96.32%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,73 +1,59 @@
 class Solution {
 public:
-    int numEnclaves(vector<vector<int>>& grid) {
-        
-        int r = grid.size();
-        int c = grid[0].size();
-        
-        // Here, we cannot covert that O's which are at boundary to X's.
-        // Thus we will do boundary traversal, find out O's and then apply bfs from that 
-        // and convert it to T.
-        // Now after that whichever O's are remaining can be converted to X's so do that
-        // and convert the T's to O's and return the answer.
-        
-        // simply push all those elements for which bfs is required into the queue.
+    int numEnclaves(vector<vector<int>>& board) {
         
         queue<pair<int,int>> q;
-        // first row
-        for(int i=0; i<c; i++) {
-            if(grid[0][i] == 1)
-            q.push({0,i});
-        }
-        // first col
-        for(int i=0; i<r; i++) {
-            if(grid[i][0] == 1)
-            q.push({i,0});
-        }
-        // last row
-        for(int i=0; i<c; i++) {
-            if(grid[r-1][i] == 1)
-            q.push({r-1,i});
-        }
-        // last col
-        for(int i=0; i<r; i++) {
-            if(grid[i][c-1] == 1)
-            q.push({i,c-1});
-        }
+        int row = board.size();
+        int col = board[0].size();
         
+
+        for(int i=0; i<col; i++) {
+            if(board[0][i] == 1) {
+                board[0][i] = 2;
+                q.push({0,i});
+            }
+            if(board[row-1][i] == 1) {
+                board[row-1][i] = 2;
+                q.push({row-1,i});
+            }
+        }
+
+        for(int j=1; j<row-1; j++) {
+            if(board[j][0] == 1) {
+                board[j][0] = 2;
+                q.push({j,0});
+            }
+            if(board[j][col-1] == 1) {
+                board[j][col-1] = 2;
+                q.push({j,col-1});
+            }
+        }
+
+        int x[4] = {0,0,1,-1};
+        int y[4] = {1,-1,0,0};
+
         while(!q.empty()) {
-            
-            auto node = q.front();
+
+            auto pair = q.front();
             q.pop();
-            
-            int i = node.first;
-            int j = node.second;
-            
-            grid[i][j] = 2;
-            
-            if(i+1 < grid.size() and grid[i+1][j] == 1) {
-                q.push({i+1,j});
-                grid[i+1][j] = 2;
-            }
-            if(j+1 < grid[i].size() and grid[i][j+1] == 1) {
-                q.push({i,j+1});
-                grid[i][j+1] = 2;
-            }
-            if(i-1 >= 0 and grid[i-1][j] == 1) {
-                q.push({i-1,j});
-                grid[i-1][j] = 2;
-            }
-            if(j-1 >= 0 and grid[i][j-1] == 1) {
-                q.push({i,j-1});
-                grid[i][j-1] = 2;
+
+            int i = pair.first;
+            int j = pair.second;
+
+            for(int k=0; k<4; k++) {
+                if(i+x[k] < row and j+y[k] < col and i+x[k] >= 0 and j+y[k] >= 0 and board[i+x[k]][j+y[k]] == 1) {
+                    board[i+x[k]][j+y[k]] = 2;
+                    q.push({i+x[k],j+y[k]});
+                }
             }
         }
-        
+
         int count = 0;
-        for(int i=0; i<grid.size(); i++) {
-            for(int j=0; j<grid[i].size(); j++) {
-                if(grid[i][j] == 1)
-                count++;
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(board[i][j] == 1) {
+                    count++;
+                }
             }
         }
 
