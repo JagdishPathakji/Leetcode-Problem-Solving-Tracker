6d915fc94bed753d740c50125b2301e74d77e5fc
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-10-15 18:53:11 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (21.55%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,62 +1,68 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
-        
-        int row = board.size();
-        int col = board[0].size();
 
         queue<pair<int,int>> q;
+        int row = board.size();
+        int col = board[0].size();
+        
+        if(row == 1 && col == 1)
+        return;
+
         for(int i=0; i<col; i++) {
-            if(board[0][i] == 'O')
-            q.push({0,i});
-            if(board[row-1][i] == 'O')
-            q.push({row-1,i});
+            if(board[0][i] == 'O') {
+                board[0][i] = 'Y';
+                q.push({0,i});
+            }
+            if(board[row-1][i] == 'O') {
+                board[row-1][i] = 'Y';
+                q.push({row-1,i});
+            }
         }
 
-        for(int i=0; i<row; i++) {
-            if(board[i][0] == 'O')
-            q.push({i,0});
-            if(board[i][col-1] == 'O')
-            q.push({i,col-1});
+        for(int j=1; j<row-1; j++) {
+            if(board[j][0] == 'O') {
+                q.push({j,0});
+                board[j][0] = 'Y';
+            }
+            if(board[j][col-1] == 'O') {
+                q.push({j,col-1});
+                board[j][col-1] = 'Y';
+            }
         }
+
+        int x[4] = {0,0,1,-1};
+        int y[4] = {1,-1,0,0};
 
         while(!q.empty()) {
 
-            auto node = q.front();
+            auto pair = q.front();
             q.pop();
 
-            int i = node.first;
-            int j = node.second;
+            int i = pair.first;
+            int j = pair.second;
 
-            board[i][j] = 'K';
+            board[i][j] = 'Y';
 
-            if(i+1 < board.size() and board[i+1][j] == 'O') {
-                q.push({i+1,j});
-                board[i+1][j] = 'K';
-            }
-
-            if(i-1 >= 0 and board[i-1][j] == 'O') {
-                q.push({i-1,j});
-                board[i-1][j] = 'K';
-            }
-
-            if(j-1 >= 0 and board[i][j-1] == 'O') {
-                q.push({i,j-1});
-                board[i][j-1] = 'K';
-            }
-
-            if(j+1 < board[i].size() and board[i][j+1] == 'O') {
-                q.push({i,j+1});
-                board[i][j+1] = 'K';
+            for(int k=0; k<4; k++) {
+                if(i+x[k] < row and j+y[k] < col and i+x[k] >= 0 and j+y[k] >= 0 and board[i+x[k]][j+y[k]] == 'O') 
+                q.push({i+x[k],j+y[k]});
             }
         }
 
         for(int i=0; i<row; i++) {
             for(int j=0; j<col; j++) {
-                if(board[i][j]  == 'K')
-                board[i][j] = 'O';
-                else if(board[i][j] == 'O')
-                board[i][j] = 'X';
+                if(board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(board[i][j] == 'Y') {
+                    board[i][j] = 'O';
+                }
             }
         }
     }
