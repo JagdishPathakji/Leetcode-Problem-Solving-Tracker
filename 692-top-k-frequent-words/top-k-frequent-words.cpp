fe5c51944ac6975bd81acf7a25808c222ad1c479
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-10-19 13:24:14 +0530

    Time: 8 ms (11.89%) | Memory: 18.2 MB (18.44%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    struct cmp {
+        bool operator()(const pair<int,string> &a, const pair<int,string> &b) {
+            if(a.first != b.first) return a.first < b.first;   // max-heap by frequency
+            return a.second > b.second;                        // tie-break: smaller lex comes first
+        }
+    };
+
+
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        
+        vector<string> ans;
+        unordered_map<string,int> mp;
+
+        for(int i=0; i<words.size(); i++) {
+            mp[words[i]]++;
+        }
+
+        priority_queue<pair<int,string>, vector<pair<int,string>>, cmp> pq;
+
+        for(auto it = mp.begin(); it != mp.end(); it++) {
+            pq.push({it->second,it->first});
+        }
+
+        while(k--) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
