167c9fe579b27303e8efb414fb55dcd06c6141b0
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-12-14 12:00:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (8.64%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,38 +2,67 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
-        int zeroFound = 0;
-        int mult = 0;
+        // 1 -1 -1 0 0
+        // 0  0 -9 3 1
+
+        // 0 0 9 0 0
+
+        // Solution - 1 (O(n) - TC and O(1) - SC)
+        // int zeroFound = 0;
+        // int mult = 0;
+
+        // for(int i=0; i<nums.size(); i++) {
+        //     if(nums[i] == 0) {
+        //         if(zeroFound >= 1) {
+        //             mult = 0;
+        //             break;
+        //         }
+        //         zeroFound += 1;
+        //     }
+        //     else {
+        //         if(mult == 0 && zeroFound <= 1)
+        //         mult = 1;
+        //         mult = mult * nums[i];
+        //     }
+        // }
+
+        // if(zeroFound) {
+        //     for(int i=0; i<nums.size(); i++) {
+        //         if(nums[i] != 0) {
+        //             nums[i] = 0;
+        //         }
+        //         else {
+        //             nums[i] = mult;
+        //         }
+        //     }
+        // }
+        // else {
+        //     for(int i=0; i<nums.size(); i++) {
+        //         nums[i] = mult / nums[i];
+        //     }
+        // }
+
+        // return nums;
+
+
+
+
+        // Solution - 2 
+        vector<int> prefixMul(nums.size());
+        vector<int> suffixMul(nums.size());
+
+        prefixMul[0] = 1;
+        for(int i=1; i<nums.size(); i++) {
+            prefixMul[i] = prefixMul[i-1] * nums[i-1];
+        }
+
+        suffixMul[nums.size()-1] = 1;
+        for(int i=nums.size()-2; i>=0; i--) {
+            suffixMul[i] = suffixMul[i+1] * nums[i+1];
+        }
 
         for(int i=0; i<nums.size(); i++) {
-            if(nums[i] == 0) {
-                if(zeroFound >= 1) {
-                    mult = 0;
-                    break;
-                }
-                zeroFound += 1;
-            }
-            else {
-                if(mult == 0 && zeroFound <= 1)
-                mult = 1;
-                mult = mult * nums[i];
-            }
-        }
-
-        if(zeroFound) {
-            for(int i=0; i<nums.size(); i++) {
-                if(nums[i] != 0) {
-                    nums[i] = 0;
-                }
-                else {
-                    nums[i] = mult;
-                }
-            }
-        }
-        else {
-            for(int i=0; i<nums.size(); i++) {
-                nums[i] = mult / nums[i];
-            }
+            nums[i] = prefixMul[i] * suffixMul[i];
         }
 
         return nums;
