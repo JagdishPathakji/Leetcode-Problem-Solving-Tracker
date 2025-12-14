e7551a330df4c0669fd422ccf6c41751b73ee053
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-12-14 11:01:16 +0530

    Time: 41 ms (32.24%) | Memory: 99.7 MB (5.65%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -2,23 +2,47 @@ class ProductOfNumbers {
 public:
 
     vector<int> v;
+    vector<int> pm;
+    int zeroIndex = -1;
     ProductOfNumbers() {
         
     }
     
     void add(int num) {
-        v.push_back(num); // O(1) TC
+        // O(1) TC to add a element
+        v.push_back(num);     
+        if(num == 0)
+        zeroIndex = v.size()-1;
+
+        if(v.size() == 1)
+        pm.push_back(num);
+        else {
+            if(pm.back() == 0)
+            pm.push_back(num);
+            else
+            pm.push_back(num * pm.back());
+        }
     }
     
     int getProduct(int k) {
-        int n = v.size()-1;
-        int ans = 1;
-        while(k--) {
-            ans *= v[n];
-            n--;
-        }
+        // O(1) TC to return product
+        if(k == 1)
+        return v.back();
+        
+        if(v.size() == k && zeroIndex == -1)
+        return pm.back();
 
-        return ans;
+        if(v.size() == k && zeroIndex != -1)
+        return 0;
+
+        if(zeroIndex != -1 && zeroIndex >= (v.size()-k))
+        return 0;
+
+        int dangerIdx = pm.size()-k-1;
+        if(pm[dangerIdx] == 0)
+        return pm.back();
+        else
+        return (pm.back()/pm[dangerIdx]);
     }
 };
 
