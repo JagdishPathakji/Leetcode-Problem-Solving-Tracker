af479ca88feb3c3365054102316a072cae8146f2
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-12-16 12:03:20 +0530

    Time: 13 ms (18.95%) | Memory: 148.7 MB (13.68%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
@@ -2,38 +2,72 @@ class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
         
+        // Solution - 1 (O(3n) TC and O(2n) SC)
+        // if(k == 0)
+        // return nums;
+
+        // int n = nums.size();
+        // vector<long long int> prefix(n+1,0);
+        // vector<long long int> suffix(n+1,0);
+
+        // prefix[0] = nums[0];
+        // suffix[n-1] = nums[n-1];
+
+        // for(int i=1; i<n; i++) {
+        //     prefix[i] = prefix[i-1] + nums[i];
+        // }
+
+        // for(int i=n-2; i>=0; i--) {
+        //     suffix[i] = suffix[i+1] + nums[i];
+        // }
+
+
+        // // prefixsum and suffixsum are ready to use.
+        // vector<int> ans;
+        // for(int i=0; i<n; i++) {
+
+        //     if(i-k >= 0 && i+k < n) {
+        //         long long int sum = 0;
+        //         if(i-k-1 < 0) {
+        //             sum = prefix[i];
+        //         }
+        //         else {
+        //             sum = prefix[i]-prefix[i-k-1];
+        //         }
+        //         sum += suffix[i+1]-suffix[i+k+1];
+        //         ans.push_back(sum/(2*k+1));
+        //     }
+        //     else {
+        //         ans.push_back(-1);
+        //     }
+        // }
+
+        // return ans;
+
+
+        // O(2n) TC and O(n) SC
         if(k == 0)
         return nums;
 
-        int n = nums.size();
-        vector<long long int> prefix(n+1,0);
-        vector<long long int> suffix(n+1,0);
+        vector<long long int> prefix(nums.size()+1,0);
 
         prefix[0] = nums[0];
-        suffix[n-1] = nums[n-1];
-
-        for(int i=1; i<n; i++) {
+        for(int i=1; i<nums.size(); i++) {
             prefix[i] = prefix[i-1] + nums[i];
         }
 
-        for(int i=n-2; i>=0; i--) {
-            suffix[i] = suffix[i+1] + nums[i];
-        }
-
-
-        // prefixsum and suffixsum are ready to use.
         vector<int> ans;
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<nums.size(); i++) {
 
-            if(i-k >= 0 && i+k+1 <= n) {
+            if(i-k >= 0 && i+k < nums.size()) {
                 long long int sum = 0;
-                if(i-k-1 < 0) {
-                    sum = prefix[i];
+                if(i-k-1 >= 0) {
+                    sum += prefix[i]-prefix[i-k-1];
                 }
                 else {
-                    sum = prefix[i]-prefix[i-k-1];
+                    sum += prefix[i];
                 }
-                sum += suffix[i+1]-suffix[i+k+1];
+                sum += prefix[i+k]-prefix[i];
                 ans.push_back(sum/(2*k+1));
             }
             else {
