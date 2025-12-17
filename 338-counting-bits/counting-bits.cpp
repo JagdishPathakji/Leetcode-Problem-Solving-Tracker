27d0ff3d9cebf4f2a6c8c2cba4e703fd20a45272
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-12-17 15:13:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (78.79%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int> ans(n+1);
+
+        for(int i=0; i<=n; i++) {
+            int num = i;
+            int count = 0;
+            while(num != 0) {
+                num = (num & (num-1));
+                count++;
+            }
+            ans[i] = count;
+        }
+
+        return ans;
+    }
+};
