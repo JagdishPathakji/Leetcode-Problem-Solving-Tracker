4b058eaa9c513e1d64e07b7619985cf27e30c1f3
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-12-20 22:15:31 +0530

    Time: 43 ms (71.43%) | Memory: 186.2 MB (100.00%) - LeetSync

--- 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.cpp
+++ 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-
-        if(nums.size() == 1)
-        return 0;
         
         unordered_map<int,bool> mp;
-        int idx = -1;
+        int idx = 0;
         for(int i=nums.size()-1; i>=0; i--) {
             if(mp.find(nums[i]) == mp.end()) {
                  mp[nums[i]] = true;   
             }
             else {
                 idx = i;
-                break;
+                return ceil((idx+1)/3.0);
             }
         }
-        
-        if(idx == -1)
-        return 0;
-        
-        return ceil((idx+1)/3.0);
+
+        return idx;
     }
 };
