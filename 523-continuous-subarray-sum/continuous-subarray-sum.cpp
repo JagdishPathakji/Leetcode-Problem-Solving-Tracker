76c2c819fb53a0ca5eacaf3975fbf40ab5aae3e0
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-02-20 17:01:17 +0530

    Time: 76 ms (65.51%) | Memory: 146.3 MB (53.72%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -4,28 +4,19 @@ public:
         
         int prefixSum = 0;
         unordered_map<int,int> mp;
+        mp[0] = -1;
 
         for(int i=0; i<nums.size(); i++) {
-
-            prefixSum += nums[i];
             
-            int rem = prefixSum  % k;
-            if(rem < 0)
-            rem += k;
+            prefixSum += nums[i];
 
-            if(rem == 0) {
-                if(i > 0)
+            if(mp.find(prefixSum%k) != mp.end()) {
+                if(i-mp[prefixSum%k] >= 2)
                 return true;
             }
 
-            if(mp.find(rem) != mp.end()) {
-                if(i-mp[rem] > 1)
-                return true;
-            }
-
-            if(mp.find(rem) == mp.end()) {
-                mp[rem] = i;
-            }
+            if(mp.find(prefixSum%k) == mp.end())
+            mp[prefixSum%k] = i;
         }
 
         return false;
