b628e5f4be6e352822efe05c56ef7b82ec125c84
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-03-02 13:18:18 +0530

    Time: 2 ms (37.54%) | Memory: 14.5 MB (53.95%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+
+        map<int,int> mp;
+
+        for(int i=0; i<trips.size();  i++) {
+
+            mp[trips[i][1]] += trips[i][0];
+            mp[trips[i][2]] -= trips[i][0]; 
+        }
+
+        int sum = 0;
+        for(auto it = mp.begin(); it != mp.end(); it++) {
+
+            sum += it->second;
+            if(sum > capacity)
+            return false;
+        }
+
+        return true;
+    }
+};
