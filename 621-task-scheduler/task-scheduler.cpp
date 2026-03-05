8a8a118ef0ca077e15b32bc704fd26dabc08072e
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-03-05 15:43:44 +0530

    Time: 86 ms (6.00%) | Memory: 38.2 MB (82.78%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int k) {
+        
+        
+        unordered_map<char,int> mp;
+        for(int i=0; i<tasks.size(); i++) {
+            mp[tasks[i]]++;
+        }
+        
+        // create priority-queue (min-heap)
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        for(auto it = mp.begin(); it != mp.end(); it++) {
+            pq.push({0,it->first});
+        }
+        
+        int timer = 0;
+        while(!pq.empty()) {
+            
+            auto peek = pq.top();
+            pq.pop();
+            
+            int reqtimer = peek.first;
+            char task = peek.second;
+            
+            if(timer >= reqtimer) {
+                mp[task]--;
+                if(mp[task] != 0) {
+                    pq.push({reqtimer+k+1,task});   
+                }
+            }
+            else {
+                pq.push({reqtimer,task});
+            }
+            
+            timer++;
+        }
+        
+        return timer;
+    
+    }
+};
