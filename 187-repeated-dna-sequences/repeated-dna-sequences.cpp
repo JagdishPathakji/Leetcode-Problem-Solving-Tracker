fea675298c28898341eb0789fbf0658b086c4fc5
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-04-03 02:46:27 +0530

    Time: 46 ms (39.12%) | Memory: 23.5 MB (64.26%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -6,19 +6,27 @@ public:
         unordered_set<string> us;
         
         string firstwindow = s.substr(0,10);
-        cout<<firstwindow<<endl;
         us.insert(firstwindow);
 
         int idx = 1;
         set<vector<string>> ans1;
+
+        // check for every window of size k and get that string
+        // after that check if string already seen, if yes store it
+        // after that if string not already seen, make it as seen using map
+        // at the end , we will have all 10len strings which are repeated atleast once.
+        // return them
+
         for(int i=k; i<s.size(); i++) {
             string str = s.substr(idx,k);
             idx++;
-            cout<<str<<endl;
+
             if(us.find(str) != us.end()) {
                 ans1.insert({str});
             }
-            us.insert(str);
+            else {
+                us.insert(str);
+            }
         }
 
         vector<string> ans;
