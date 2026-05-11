bd7d8b42a237ade3fd37372067a2e074f5cc6146
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-05-11 19:03:53 +0530

    Time: 7 ms (35.43%) | Memory: 16.1 MB (29.64%) - LeetSync

---
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        
+        vector<int>ans;
+
+        for(int i = 0;i<nums.size();i++)
+        {
+            vector<int>temp;
+            while(nums[i] > 0)
+            {
+                temp.push_back(nums[i]%10);
+                nums[i] = nums[i]/10;
+            }
+            for(int j = temp.size()-1;j>=0; j--)
+            {
+                ans.push_back(temp[j]);
+            }   
+
+        }
+
+        return ans;
+        
+    }
+};
