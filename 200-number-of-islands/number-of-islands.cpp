9329290468f29f9ad03639d776045a889f5bd825
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-05-30 09:06:48 +0530

    Time: 30 ms (33.79%) | Memory: 18.3 MB (39.08%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,35 +1,49 @@
 class Solution {
 public:
-
-    int x[4] = {0,0,1,-1};
-    int y[4] = {1,-1,0,0};
-
-    void DFS(int i, int j, vector<vector<char>> &grid, vector<vector<bool>> &visited) {
-
-        visited[i][j] = true;
-
-        for(int k=0; k<4; k++) {
-            if(i+x[k] < grid.size() && j+y[k] < grid[0].size()) {
-                if(!visited[i+x[k]][j+y[k]] && grid[i+x[k]][j+y[k]] == '1')
-                DFS(i+x[k],j+y[k],grid,visited);
-            }
-        }
-    }
-
     int numIslands(vector<vector<char>>& grid) {
         
-        int n = grid.size();
-        int m = grid[0].size();
-       
-        vector<vector<bool>> visited(n, vector<bool>(m,false));
         int count = 0;
+        queue<pair<int,int>> q;
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(!visited[i][j] && grid[i][j] == '1') {
-                    DFS(i,j,grid,visited);
+        for(int i=0; i<grid.size(); i++) {
+            for(int j=0; j<grid[i].size(); j++) {
+
+                if(grid[i][j] == '1') {
+
+                    grid[i][j] = '0';
+                    q.push({i,j});
+                    
+                    while(!q.empty()) {
+                        
+                        auto node = q.front();
+                        int r = node.first;
+                        int c = node.second;
+                        q.pop();
+
+                        if(r + 1 < grid.size() && grid[r+1][c] == '1') {
+                            grid[r+1][c] = '0';
+                            q.push({r+1,c});
+                        }
+
+                        if(c + 1 < grid[i].size() && grid[r][c+1] == '1') {
+                            grid[r][c+1] = '0';
+                            q.push({r,c+1});
+                        }
+
+                        if(r - 1 >= 0 && grid[r-1][c] == '1') {
+                            grid[r-1][c] = '0';
+                            q.push({r-1,c});
+                        }
+
+                        if(c - 1 >= 0 && grid[r][c-1] == '1') {
+                            grid[r][c-1] = '0';
+                            q.push({r,c-1});
+                        }
+                    }
+
                     count++;
                 }
+
             }
         }
 
