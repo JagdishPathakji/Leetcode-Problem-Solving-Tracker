238d23528a984a641ac07ebbc59173b125c6c1c0
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-05-30 10:37:26 +0530

    Time: 1 ms (17.84%) | Memory: 17.3 MB (92.72%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
-
-    bool DFS(int node, char c, vector<vector<int>> &graph, vector<bool> &visited, vector<char> &color) {
-
-        visited[node] = true;
-        color[node] = c;
-        c = c == 'R' ? 'B' : 'R'; 
-
-        for(int i=0; i<graph[node].size(); i++) {
-            if(!visited[graph[node][i]]) {
-                if(!DFS(graph[node][i],c,graph,visited,color))
-                return 0;
-            }
-            else {
-                if(color[node] == color[graph[node][i]])
-                return 0;
-            }
-        }
-
-        return 1;
-    }
-
     bool isBipartite(vector<vector<int>>& graph) {
         
-        // here we are provided with the adjacency list
         int n = graph.size();
-        vector<char> color(n);
-        vector<bool> visited(n,false);
+        int m = graph[0].size();
 
+        vector<bool> visited(n,false);
+        vector<char> color(n);
+        
+        queue<int> q;
         for(int i=0; i<n; i++) {
-            if(!visited[i] and !DFS(i,'R',graph,visited,color))
-            return 0;
+
+            if(!visited[i]) {
+
+                q.push(i);
+                visited[i] = true;
+                color[i] = 'R';
+
+                while(!q.empty()) {
+                    auto node = q.front();
+                    q.pop();
+
+                    for(int j=0; j<graph[node].size(); j++) {
+                        if(visited[graph[node][j]] == true && color[graph[node][j]] == color[node])
+                        return false;
+
+                        if(visited[graph[node][j]])
+                        continue;
+
+                        visited[graph[node][j]] = true;
+
+                        if(color[node] == 'R')
+                        color[graph[node][j]] = 'B';
+                        else if(color[node] == 'B')
+                        color[graph[node][j]] = 'R';
+
+                        q.push(graph[node][j]);
+                    }
+                }
+            }
         }
 
-        return 1;
+        return true;
+
     }
 };
