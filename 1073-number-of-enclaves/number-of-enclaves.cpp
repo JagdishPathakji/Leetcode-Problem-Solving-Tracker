ad91641a4f249ed2e538a665107aa1f61b5242d7
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-05-30 10:14:37 +0530

    Time: 8 ms (61.86%) | Memory: 28.6 MB (99.46%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -3,60 +3,61 @@ public:
     int numEnclaves(vector<vector<int>>& board) {
         
         queue<pair<int,int>> q;
-        int row = board.size();
-        int col = board[0].size();
-        
+        int n = board.size();
+        int m = board[0].size();
 
-        for(int i=0; i<col; i++) {
+        for(int i=0; i<n; i++) {
+            if(board[i][0] == 1) {
+                board[i][0] = 2;
+                q.push({i,0});
+            }
+            if(board[i][m-1] == 1) {
+                board[i][m-1] = 2;
+                q.push({i,m-1});
+            }
+        }
+
+        for(int i=0; i<m; i++) {
             if(board[0][i] == 1) {
                 board[0][i] = 2;
                 q.push({0,i});
             }
-            if(board[row-1][i] == 1) {
-                board[row-1][i] = 2;
-                q.push({row-1,i});
+            if(board[n-1][i] == 1) {
+                board[n-1][i] = 2;
+                q.push({n-1,i});
             }
         }
 
-        for(int j=1; j<row-1; j++) {
-            if(board[j][0] == 1) {
-                board[j][0] = 2;
-                q.push({j,0});
-            }
-            if(board[j][col-1] == 1) {
-                board[j][col-1] = 2;
-                q.push({j,col-1});
-            }
-        }
-
-        int x[4] = {0,0,1,-1};
-        int y[4] = {1,-1,0,0};
+        int rarr[] = {1,-1,0,0};
+        int carr[] = {0,0,1,-1};
 
         while(!q.empty()) {
 
-            auto pair = q.front();
+            auto node = q.front();
             q.pop();
 
-            int i = pair.first;
-            int j = pair.second;
+            int r = node.first;
+            int c = node.second;
 
-            for(int k=0; k<4; k++) {
-                if(i+x[k] < row and j+y[k] < col and i+x[k] >= 0 and j+y[k] >= 0 and board[i+x[k]][j+y[k]] == 1) {
-                    board[i+x[k]][j+y[k]] = 2;
-                    q.push({i+x[k],j+y[k]});
+            for(int i=0; i<4; i++) {
+                int newr = rarr[i] + r;
+                int newc = carr[i] + c;
+
+                if(newr >= 0 && newc >= 0 && newr < n && newc < m && board[newr][newc] == 1) {
+                    board[newr][newc] = 2;
+                    q.push({newr,newc});
                 }
             }
         }
 
         int count = 0;
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
-                if(board[i][j] == 1) {
-                    count++;
-                }
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(board[i][j] == 1)
+                count++;
             }
         }
 
-        return count;
+        return count;        
     }
 };
