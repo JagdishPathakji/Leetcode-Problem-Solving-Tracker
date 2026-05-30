748a7a46df0c0e41ec035f10c44450512cf982a6
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-05-30 10:11:58 +0530

    Time: 88 ms (33.03%) | Memory: 112.9 MB (72.74%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) {
+
+        for(int i=0; i<mat.size(); i++) {
+            for(int j=0; j<mat[i].size(); j++) {
+                mat[i][j] = !mat[i][j];
+            }
+        }
+
+        vector<vector<int>> ans(mat.size(), vector<int>(mat[0].size()));
+        queue<pair<int,int>> q;
+
+        for(int i=0; i<mat.size(); i++) {
+            for(int j=0; j<mat[i].size(); j++) {
+                if(mat[i][j] == 0) {
+                    q.push({i,j});
+                    ans[i][j] = 0;
+                }
+            }
+        }
+
+        int rarr[] = {0,0,1,-1};
+        int carr[] = {1,-1,0,0};
+
+
+        int target = 1;
+        while(!q.empty()) {
+
+            int size = q.size();
+            while(size--) {
+
+                auto node = q.front();
+                q.pop();
+                int r = node.first;
+                int c = node.second;
+
+                for(int i=0; i<4; i++) {
+                    int newr = rarr[i] + r;
+                    int newc = carr[i] + c;
+
+                    if(newr >= 0 && newc >= 0 && newr < mat.size() && newc < mat[r].size() && mat[newr][newc] == 1) {
+                        mat[newr][newc] = 0;
+                        ans[newr][newc] = target;
+                        q.push({newr,newc});
+                    }
+                }
+            }
+
+            target++;
+        }
+
+        return ans;
+    }
+};
